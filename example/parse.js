9df0f1a2eaa9038faafd6d0941b5fd9cd14a5410
dogukan6durukan <dogukandurukan0506@gmail.com>
2025-08-03 09:38:48 +0300

    dump

--- example/parse.js
+++ example/parse.js
@@ -3,7 +3,21 @@ import { Parser } from "../index.js";
 let cls = new Parser("index.htl","example", {
     title : "Admin Page",
     user_name : "Dogukan",
-    age : 19
+    age : 19,
+    books : [
+        {
+            title : "foo1",
+            author: "bar1"
+        },
+        {
+            title : "foo2",
+            author: "bar2"
+        },
+        {
+            title : "foo3",
+            author: "bar3"
+        },
+    ]
 });
 
 await cls.getSource();
