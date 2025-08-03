a176bb0187446e018ee4457044b3fb38c2f09804
dogukan6durukan <dogukandurukan0506@gmail.com>
2025-08-03 09:13:34 +0300

    change

--- rules.js
+++ rules.js
@@ -1,5 +1,6 @@
 export const RULES = {
     inc_directive : /#include *([A-Z|a-z|0-9\/].+)\.htl/g,
-    file_extension : /(?!include\b)\b\w+([A-Z|a-z|0-9\/].+)\.htl/,
-    variable : /({{)([A-Z|a-z].+?)(}})/
+    file_extension : /(?!include\b)\b\w+([A-Z|a-z|0-9\/].+)\.htl/g,
+    variable : /({{)([A-Z|a-z].+?)(}})/,
+    dump : /@dump *([A-Z|a-z].+)\{(.*)}/
 }
